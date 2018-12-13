d883706656a452a3288bd351129b26a05ca26429
Travis Martin <travisemerymartin@gmail.com>
2018-12-13 21:45:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  var result = document.querySelector(selector)
+  return result
+}
 
+function nestedTarget() {
+  var aim = document.querySelector('div#nested div.target')
+  return aim
+}
+
+function increaseRankBy(n) {
+  var search = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i < search.length; i++) {
+    search[i].innerHTML = parseInt(search[i].innerHTML) + parseInt(n)
+  }
+}
+
+function deepestChild() {
+  var grand = document.getElementById('app').querySelector('div#grand-node')
+  if (grand.querySelector('div') != undefined) {
+    var grand = grand + ".querySelector('div')"
+  }
+  else {
+    return grand
+  }
+}
